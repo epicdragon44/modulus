8711a59fd699093dd79b8cf667a012bb804f6546
Haoli <haoliyin2002@gmail.com>
2020-05-24 05:00:54 -0600

    testing heroku

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,6 +1,9 @@
 import app from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
+
+console.log(process.env.REACT_APP_API_KEY);
+
 const config = {
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: "modulus-e56e4.firebaseapp.com",
